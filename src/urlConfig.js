06c18ad17b95bfc04f2899a877ebe445c7b66d02
Onaneye Ajibola <ajibolaonaneye@gmail.com>
2021-02-21 17:42:35 +0100

    fourth Commit

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,6 +1,6 @@
 const baseUrl = `https://shopper-back-server.herokuapp.com`
 
-export const api = baseUrl;
+export const api = `${baseUrl}/api`;
 
 export const generatePublicUrl = (fileName) => {
   return `${baseUrl}/public/${fileName}`;
